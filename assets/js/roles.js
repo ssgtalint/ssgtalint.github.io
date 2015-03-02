caf534e78d0fd046ac2baea7cb561eb44ade3fee
Nathan Pointer <nathanpointer@Nathans-MacBook-Pro.local>
2015-03-01 20:23:47 -0800

    full account based info hiding implemented

--- assets/js/roles.js
+++ assets/js/roles.js
@@ -1,9 +1,35 @@
-// $(function(){
+// manager-only
+// hr-only
+// employee-only
+
+console.log(getCookie("account_type"));
+
+if(getCookie("account_type") != 'hr'){
+	$('.hr-only').hide();
+}
+
+if(getCookie("account_type") != 'manager'){
+	$('.manager-only').hide();
+}
+
+if(getCookie("account_type") != 'employee'){
+	$('.employee-only').hide();
+}
+
+
+if(getCookie("account_type") == 'hr'){
+	$('.not-hr').hide();
+}
+
+if(getCookie("account_type") == 'manager'){
+	$('.not-manager').hide();
+}
+
+if(getCookie("account_type") == 'employee'){
+	$('.not-employee').hide();
+}
+
+$('#user-role-display').html(" | " + getCookie("account_type"));
 
 
 
-// });
-
-// alert('roles');
-
-console.log('foo');
