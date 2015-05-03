79e04b0523b2a31f06ab8b3e2a1654a3e60c29c2
Nathan Pointer <nathanpointer@Nathans-MacBook-Pro.local>
2015-05-02 20:59:35 -0700

    basic alert based version working

--- assets/js/protectAccess.js
+++ assets/js/protectAccess.js
@@ -1,9 +1,7 @@
-
 var token = getCookie("ua_session_token");
 
 if(token){
 	UserApp.setToken(token);
-
 
 	UserApp.User.get({ user_id: "self" }, function(error, user) {
         if (error) {
@@ -13,7 +11,22 @@ if(token){
                 window.location.href = "/login/";
             }
         } else {
-            // Success, the profile is at user[0]
+            valid = $.inArray( user[0].email, users ) >= 0;
+
+            if(!valid){
+               if(window.location != '/logout'){
+                 alert('you are not on the user list, to request access please contact adfasdf@gmail.com');
+               }
+                window.location.href = "/logout";
+            }
+
+
+            if(user[0].properties.user_type.value == "" &&
+                location.pathname != '/edit/'
+            ){
+                alert('please complete your user profile');
+                window.location.href = "/edit";
+            }
         }
     });
 }else {
